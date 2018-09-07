e8edad8103ca6ab57626661d8d30e507af237764
HOME <hhw1990@naver.com>
2018-09-08 06:44:39 +0900

    mail_function Update

--- _js/scripts.js
+++ _js/scripts.js
@@ -242,7 +242,7 @@ $( "#contactForm" ).submit( function( e ) {
     $.ajax( {
 
         // Change the email address here:
-        url: "https://formspree.io/jan.czizikow@Fmail.com",
+        url: "https://formspree.io/hhw1990@naver.com",
         method: "POST",
         data: $( this ).serialize(),
         dataType: "json",
